dbdb4fe264ada28a1c054e7196d2f37eb5ae4998
James Corey <james@bendyworks.com>
2018-03-24 14:56:30 -0500

    Refactored and added some more tests

--- app/models/financial_summary.rb
+++ app/models/financial_summary.rb
@@ -1,47 +1,47 @@
 class FinancialSummary
-
-  attr_accessor :user, :currency, :created_at
+  attr_accessor :user_id, :currency, :date_range, :created_at
 
   def self.one_day(params)
     summary = new
-
-    summary.user = params[:user]
-    summary.currency = params[:currency].to_s.upcase
-    summary.created_at = (Time.now - 1)...Time.now
-
-    summary
+    summary.summary_builder(summary, params)
+    summary.date_range = (Time.now - 1)...Time.now
+    summary if summary.valid_fields?(summary)
   end
 
   def self.seven_days(params)
     summary = new
-
-    summary.user = params[:user]
-    summary.currency = params[:currency].to_s.upcase
-    summary.created_at = (Date.today - 7)...Date.today
-
-    summary
+    summary.summary_builder(summary, params)
+    summary.date_range = (Date.today - 7)...Date.today
+    summary if summary.valid_fields?(summary)
   end
 
   def self.lifetime(params)
     summary = new
-
-    summary.user = params[:user]
-    summary.currency = params[:currency].to_s.upcase
-    summary.created_at = (Date.today - 100.years)...Date.today
-
-    summary
+    summary.summary_builder(summary, params)
+    summary.date_range = (Date.today - 100.years)...Date.today
+    summary if summary.valid_fields?(summary)
   end
 
   def count(category)
-    Transaction.where(user: user, category: category, created_at: created_at, amount_currency: currency).count
+    Transaction.where(user_id: user_id, category: category, created_at: date_range, amount_currency: currency).count
   end
 
   def amount(category)
-    transactions = Transaction.where(user: user, category: category, created_at: created_at, amount_currency: currency)
+    transactions = Transaction.where(user_id: user_id, category: category, created_at: date_range, amount_currency: currency)
     total = 0
     transactions.each do |t|
       total += t.amount
     end
     total
+  end
+
+  def summary_builder(summary, params)
+    summary.user_id = params[:user].id if params[:user]
+    summary.currency = params[:currency].to_s.upcase if params[:currency]
+    summary.created_at = Time.now
+  end
+
+  def valid_fields?(summary)
+    summary.user_id && summary.currency && summary.date_range && summary.created_at
   end
 end
--- spec/models/financial_summary_spec.rb
+++ spec/models/financial_summary_spec.rb
@@ -19,6 +19,30 @@ describe FinancialSummary do
     expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
   end
 
+  it 'only selects transactions from the asked category' do
+    user = create(:user)
+
+    Timecop.freeze(Time.now) do
+      create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(12.12, :usd))
+      create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(10, :usd))
+    end
+
+    Timecop.freeze(Time.now) do
+      create(:transaction, user: user, category: :refund, amount: Money.from_amount(1.10, :usd))
+    end
+
+    subject = FinancialSummary.one_day(user: user, currency: :usd)
+
+    expect(subject.count(:withdraw)).to eq(2)
+    expect(subject.amount(:withdraw)).to eq(Money.from_amount(22.12, :usd))
+
+    expect(subject.count(:refund)).to eq(1)
+    expect(subject.amount(:refund)).to eq(Money.from_amount(1.10, :usd))
+
+    expect(subject.count(:deposit)).to eq(0)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(0.00, :usd))
+  end
+
   it 'summarizes over seven days' do
 
     user = create(:user)
@@ -53,5 +77,16 @@ describe FinancialSummary do
     subject = FinancialSummary.lifetime(user: user, currency: :usd)
     expect(subject.count(:deposit)).to eq(3)
     expect(subject.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
+  end
+
+  it 'ensures FinacialSummary has valid fields' do
+    user = create(:user)
+
+    Timecop.freeze(Time.now) do
+      create(:transaction, user: user, category: :deposit)
+    end
+
+    expect(FinancialSummary.one_day(currency: :usd)).to be_nil
+    expect(FinancialSummary.one_day(user: user, currency: :usd)).not_to be_nil
   end
 end
